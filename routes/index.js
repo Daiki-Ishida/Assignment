8687daf168437b5dbebb91cbc559dad3a6eb432e
Daiki <missoula0132@gmail.com>
2019-09-15 01:08:11 +0900

    create views for register/login form

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register');
 });
 
 module.exports = router;
