e26cc81508007019434d08d335fa8c0a891f811c
Daiki <missoula0132@gmail.com>
2019-09-16 17:48:37 +0900

    setup csrf prevention

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', (req, res, next) => {
-  req.session.uid = '';
+  delete req.session.uid;
   res.redirect('/');
 })
 
--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+// csrf
+const Tokens = require("csrf");
+const tokens = new Tokens();
+const setToken = async (req, res) => {
+  const secret = await tokens.secretSync();
+  const token = await tokens.create(secret);
+  req.session.csrf = secret;
+  res.cookie('csrf', token);
+}
+const checkToken = async (req, res) => {
+  const secret = await req.session.csrf;
+  const token = await req.cookies.csrf;
+  if (tokens.verify(secret, token) === false) {
+    throw new Error("Invalid Token");
+  }
+  console.log('varified!')
+  delete req.session.csrf;
+  res.clearCookie('csrf');
+}
+
 var models = require('../models');
 
 router.get('/', function (req, res, next) {
+  setToken(req, res);
   res.render('register');
 });
 
 router.post('/', (req, res, next) => {
+  checkToken(req, res);
   models.User.create({
     name: req.body.name,
     email: req.body.email,
